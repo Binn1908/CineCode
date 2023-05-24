5beb1c966a4968f1aaf0e2f9a4fc84199e924872
Binn1908 <126502493+Binn1908@users.noreply.github.com>
2023-05-24 16:11:11 +0200

    Update tab_ressources.py

--- tab_ressources.py
+++ tab_ressources.py
@@ -3,11 +3,11 @@ import streamlit as sl
 def tab_ressources():
 	sl.title('Ressources')
 
-	sl.subheader('IMDb')
+	sl.write("<span style='font-size: 29px; color: #F8B405;'>IMDb</span>", unsafe_allow_html=True)
 	sl.write('[Les datasets](https://datasets.imdbws.com/)')
 	sl.write('[Documentation](https://developer.imdb.com/non-commercial-datasets/)')
 
-	sl.subheader('Code')
+	sl.write("<span style='font-size: 29px; color: #F8B405;'>Code</span>", unsafe_allow_html=True)
 
 	col1, col2 = sl.columns([1,2])
 	sl.write('- Exploration des données IMDb')

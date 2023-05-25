9ce7e34c4c9b8184f3a9ffd9fb97cb95f55e3c2a
Binn1908 <126502493+Binn1908@users.noreply.github.com>
2023-05-25 16:17:06 +0200

    Update tab_cinecode.py

--- tab_cinecode.py
+++ tab_cinecode.py
@@ -13,15 +13,15 @@ def tab_cinecode():
 	with tab1:
 		sl.subheader("L'équipe")
 		col1, col2, col3 = sl.columns([1,1,1])
-		col1.write('Chinnawat')
-		col2.write('Jacques')
-		col3.write('Khalid')
+		col1.write("Chinnawat")
+		col2.write("Jacques")
+		col3.write("Khalid")
 
-		sl.subheader('Mission')
+		sl.subheader("Mission")
 		sl.write("- Création d'un outil de recommandation de films pour un cinéma dans la Creuse souhaitant passer le cap du digital")		
 
 	with tab2:
-		sl.subheader('Notre démarche')
+		sl.subheader("Notre démarche")
 		sl.write("- **Exploration et nettoyage de la BDD IMDb** afin de proposer aux locaux de la Creuse une sélection de films pertinents :")
 		sl.write("	- éliminer les données inutiles")
 		sl.write("	- corriger les erreurs")
@@ -30,20 +30,20 @@ def tab_cinecode():
 		sl.write("- **Utilisation d'un algorithme de machine learning** pour recommander des films en fonction de films qui ont été appréciés par le spectateur")
 		sl.write("- **Création d’une interface utilisateur conviviale** en utilisant Streamlit pour présenter les propositions")
 			 			
-		sl.subheader('Outils utilisés')
+		sl.subheader("Outils utilisés")
 		sl.write("- Analyse et nettoyage des données : Pandas, Matplotlib")
 		sl.write("- Machine learning : scikit-learn")
 		sl.write("- Expérience utilisateur : Streamlit, IMDb API")
 
 	with tab3:
-		sl.subheader('Machine Learning')
-		sl.write("- Utilisation de l’algorithme **KNN** (K plus proches voisins) en raison de sa capacité à trouver des films similaires en fonction des préférences de l’utilisateur en se basant sur les caractéristiques des films et en identifiant les voisins les plus proches dans l’espace de caractéristiques.")
+		sl.subheader("Machine Learning")
+		sl.write("- Utilisation de l’algorithme **KNN** (K plus proches voisins) en raison de sa capacité à trouver des films similaires en fonction des préférences de l’utilisateur en se basant sur les caractéristiques des films et en identifiant les voisins les plus proches dans l’espace de caractéristiques")
 
 	with tab4:
-		sl.subheader('Difficultés rencontrées')
-		sl.write('- Volume de base de données important')
-		sl.write('- Filtrer la base de données sans perdre des films cultes/classiques')
-
+		sl.subheader("Difficultés rencontrées")
+		sl.write("- Filtrer la base de données IMDb sans perdre des films cultes/classiques")
+		sl.write("- Volume du dataset final important : ralentissement du moteur de recommandation")
+		
 		sl.subheader("Points d'amélioration")
 		sl.write("- Pour affiner les résultats : retravailler le dataset avec plus de choix et plus d'acteurs")
 		sl.write("- Traduction des synopsis en français")
